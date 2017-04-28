c6a124ba23d992fdcfd254e0af4c56475eec2ac1
mpalliser <mariano.palliser@gmail.com>
2017-04-28 10:24:40 +0200

    refactor controller

--- practica_setmana_1/src/main/java/org/formacio/mvc/ControladorAlumnat.java
+++ practica_setmana_1/src/main/java/org/formacio/mvc/ControladorAlumnat.java
@@ -5,18 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mariano.palliser on 28/04/2017.
  */
-@RestController
+@Controller
 public class ControladorAlumnat {
 
 	@Autowired
 	ServeiAlumnat alumnatService;
 
 	@RequestMapping(path="/alumnes")
+	@ResponseBody
 	public String mostrarAlumnes() {
 		return String.valueOf(alumnatService.alumnesRegistrats());
 	}
